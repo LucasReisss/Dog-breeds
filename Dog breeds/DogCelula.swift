9ea2d6e4267849d21b017eb2af4940f80af88b8d
LucasReisss <lucasbarroreis@gmail.com>
2022-04-23 17:13:57 -0300

    dog breeds

---
+++ Dog breeds/DogCelula.swift
@@ -0,0 +1,15 @@
+//
+//  DogCelula.swift
+//  Dog breeds
+//
+//  Created by Lucas da Silva Reis on 22/04/22.
+//
+
+import UIKit
+
+class DogCelula: UITableViewCell {
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var descricaoLabel: UILabel!
+}
